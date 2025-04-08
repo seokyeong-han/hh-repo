db9ed3695282e442aadb7d800fd872ee5dc2cf16
seokyeong-han <tjrdud3347@gmail.com>
2025-04-08 20:02:51 +0900

    3주차 프로젝트 셋팅

---
+++ 3week/e-commerce_3week/src/test/java/com/example/ecommerce_3week/ECommerce3weekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_3week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerce3weekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
