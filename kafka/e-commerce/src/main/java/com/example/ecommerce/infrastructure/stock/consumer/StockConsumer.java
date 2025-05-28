450b2bae5b0615e065962452f7f549dd738cb530
seokyeong-han <tjrdud3347@gmail.com>
2025-05-29 01:08:11 +0900

    주문 이벤트 소비 시 재고차감 서비스 호출 로직 업데이트

--- kafka/e-commerce/src/main/java/com/example/ecommerce/infrastructure/stock/consumer/StockConsumer.java
+++ kafka/e-commerce/src/main/java/com/example/ecommerce/infrastructure/stock/consumer/StockConsumer.java
@@ -1,12 +1,14 @@
 package com.example.ecommerce.infrastructure.stock.consumer;
 
-import com.example.ecommerce.api.order.controller.OrderController;
 import com.example.ecommerce.domain.event.OrderStartEvent;
+import com.example.ecommerce.domain.event.StockSuccessEvent;
+import com.example.ecommerce.domain.stock.service.StockService;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,13 +16,31 @@ import org.springframework.stereotype.Component;
 public class StockConsumer {
     private static final Logger log = LoggerFactory.getLogger(StockConsumer.class);
 
-    @KafkaListener(topics = "order.start", groupId = "stock-service", containerFactory = "orderKafkaListenerContainerFactory")
+    private final StockService stockService;
+    private final KafkaTemplate<String, StockSuccessEvent> kafkaTemplate;
+
+    @KafkaListener(
+            topics = "order.start",
+            groupId = "stock-service",
+            containerFactory = "orderKafkaListenerContainerFactory"
+    )
     public void consume(ConsumerRecord<String, OrderStartEvent> record) {
         OrderStartEvent event = record.value();
         String orderId = record.key(); // 또는 event.getOrderId() 사용 가능
 
         log.info("✅ [StockService] Received OrderStartEvent (orderId={}): {}", orderId, event);
-
         log.info("✅ Stock reduction completed for orderId={}", orderId);
+
+        //재고 차감 service 호출
+        try {
+            stockService.reduceStock(record.value(), record.key());
+            kafkaTemplate.send("stock.success", orderId, new StockSuccessEvent(
+                    orderId, event.userId(), event.items()
+            ));
+        } catch (Exception e) {
+            log.error("재고 차감 실패: {}", e.getMessage());
+        }
+
+
     }
 }
