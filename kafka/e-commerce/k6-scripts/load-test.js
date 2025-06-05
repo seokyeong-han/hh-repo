96c52d2e8d909921fa0786208223aa0b64c47c45
hanseok <hanseok@ihopper.co.kr>
2025-06-05 16:52:31 +0900

    부하 테스트 업로드

--- kafka/e-commerce/k6-scripts/load-test.js
+++ kafka/e-commerce/k6-scripts/load-test.js
@@ -3,22 +3,34 @@ import { check, sleep } from 'k6';
 import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 export let options = {
-    scenarios: { //10초동안 100명의 사용자가 요청반복
-        peak_load: {
-            executor: 'constant-vus',
-            vus: 100, // 동시 사용자 수
-            duration: '10s', // 짧은 시간에 몰아치기
-        }
-    }
+    stages: [
+        { duration: '2s', target: 0 },    // 준비
+        { duration: '1s', target: 1000 },  // 급증
+        { duration: '10s', target: 300 }, // 유지
+        { duration: '5s', target: 0 },    // 감소
+    ]
 };
 
 export default function () {
-    const url = 'http://host.docker.internal:8080/api/orders';
+    //상품 조회
+    let orderUrl = 'http://host.docker.internal:8080/api/products';
+    const res1 = http.get(orderUrl);
+    check(res1, { '상품 조회 성공 status is 200': (r) => r.status === 200 });
 
-    const payload = JSON.stringify({
+    //인기 상품 조회
+    let popOrderUrl = 'http://host.docker.internal:8080/api/popularProduct/today';
+    const res2 = http.get(popOrderUrl);
+    check(res2, { '인기 상품 조회 성공 status is 200': (r) => r.status === 200 });
+
+    //주문
+    const url = 'http://host.docker.internal:8080/api/orders';
+    const payload  = JSON.stringify({
         userId: randomIntBetween(1, 500),
         items: [
-            { productId: 10, quantity: 1 }
+            {
+                productId: randomIntBetween(1, 10)
+                ,quantity: 1
+            }
         ]
     });
 
@@ -30,8 +42,13 @@ export default function () {
 
     const res = http.post(url, payload, params);
 
+    // 응답 코드가 4xx 또는 5xx면 실패로 기록
+    if (res.status >= 400) {
+        errorCount.add(1);
+    }
+
     check(res, {
-        'status is 200': (r) => r.status === 200,
+        '주문 성공 status is 200': (r) => r.status === 200,
     });
 
     sleep(1); // 각 유저가 1초 대기
