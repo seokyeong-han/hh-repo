300c7ea9e83266779f912beb7de6ec40b4e4cc1a
hanseok <hanseok@ihopper.co.kr>
2025-06-05 15:09:08 +0900

    캐시 사용 main 업데이트

--- kafka/e-commerce/src/main/java/com/example/ecommerce/ECommerceApplication.java
+++ kafka/e-commerce/src/main/java/com/example/ecommerce/ECommerceApplication.java
@@ -2,8 +2,10 @@ package com.example.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching //캐시 사용 설정
 public class ECommerceApplication {
 
     public static void main(String[] args) {
